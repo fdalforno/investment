2a6512687c233383551bb19d32931f5a78b3e721
dalfornof <dalfornof@corp.generali.net>
2024-10-03 14:42:31 +0200

    Primo Commit

---
+++ risk.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import norm
+
+def drawdown(return_series: pd.Series) -> pd.DataFrame:
+    """Takes a time series of asset returns.
+       returns a DataFrame with columns for
+       the wealth index, 
+       the previous peaks, and 
+       the percentage drawdown
+    """
+    wealth_index = 1000*(1+return_series).cumprod()
+    previous_peaks = wealth_index.cummax()
+    drawdowns = (wealth_index - previous_peaks)/previous_peaks
+    return pd.DataFrame({"Wealth": wealth_index, 
+                         "Previous Peak": previous_peaks, 
+                         "Drawdown": drawdowns})
+
+
+
+def var_gaussian(r, level=5, modified=False):
+    """
+    Returns the Parametric Gaussian VaR of a Series or DataFrame
+    If "modified" is True, then the modified VaR is returned,
+    using the Cornish-Fisher modification
+    """
+    # compute the Z score assuming it was Gaussian
+    z = norm.ppf(level/100)
+    if modified:
+        # modify the Z score based on observed skewness and kurtosis
+        s = r.skew()
+        k = r.kurtosis()
+        z = (z +
+                (z**2 - 1)*s/6 +
+                (z**3 -3*z)*(k-3)/24 -
+                (2*z**3 - 5*z)*(s**2)/36
+            )
+    return -(r.mean() + z*r.std(ddof=0))
