b0619093d4ad76edc5cca0d661e759632b985084
dalfornof <dalfornof@corp.generali.net>
2024-11-29 09:00:05 +0100

    factor investing

--- dataset.py
+++ dataset.py
@@ -41,3 +41,17 @@ def get_ind_size():
     Load and format the Ken French 30 Industry Portfolios Average size (market cap)
     """
     return get_ind_file("size")
+
+
+def get_total_market_index_returns():
+    """
+    Load the 30 industry portfolio data and derive the returns of a capweighted total market index
+    """
+    ind_nfirms = get_ind_nfirms()
+    ind_size = get_ind_size()
+    ind_return = get_ind_returns()
+    ind_mktcap = ind_nfirms * ind_size
+    total_mktcap = ind_mktcap.sum(axis=1)
+    ind_capweight = ind_mktcap.divide(total_mktcap, axis="rows")
+    total_market_return = (ind_capweight * ind_return).sum(axis="columns")
+    return total_market_return
